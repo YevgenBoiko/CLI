b25a3994fd08cb23f529974e5989a0af6ba28bee
YevgenBoiko <yevheniiboiko@gmail.com>
2023-05-03 19:50:23 +0200

    added CLI

---
+++ README.md
@@ -0,0 +1,6 @@
+##ScreenShots
+
+1. https://monosnap.com/file/7FM4bTjFgePX7ShuHpujwxW2Vza2kx
+2. https://monosnap.com/file/gTI0yOpWONKuZy2LcxMmPH0TOgo8jE
+3. https://monosnap.com/file/AMToVnHY4i714ktL4GxMsadgI0c1cm
+4. https://monosnap.com/file/0FP6Y0Gdt4p4qOIcHiX8bL4DLnLlRp
---
+++ index.js
@@ -0,0 +1,23 @@
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      return console.log(removedContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
